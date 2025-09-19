7fce34cbb2b57e3cde324c65ad5a18d16ec5acc5
khristines238 <khristines238@gmail.com>
2025-09-19 18:47:15 +0800

    I add new file for the MyThreadClass

---
+++ ThreadsPriority/MyThreadClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadsPriority
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            Thread thread = Thread.CurrentThread;
+            for (int loopCount = 0; loopCount < 2; loopCount++)
+            {
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + loopCount);
+
+                Thread.Sleep(500);
+
+            }
+        }
+        public static void Thread2()
+        {
+            Thread thread = Thread.CurrentThread;
+            for (int loopCount = 0; loopCount < 6; loopCount++)
+            {
+                Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + loopCount);
+
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
